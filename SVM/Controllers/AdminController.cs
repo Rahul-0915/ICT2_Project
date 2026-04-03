f38360792a1fdee543e8831f22f9b96bb373e581
pikachu1509 <bhagvatrahul0915@gmail.com>
2026-04-03 20:19:21 +0530

    Add project files.

---
+++ SVM/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SVM.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ SVM/wwwroot/js/AdminPanel.js
@@ -0,0 +1,15 @@
+﻿window.onload = function () {
+    document.getElementById("mainFrame").src = "/Admin/AdminDashboard";
+};
+function toggleMenu(menuId) {
+    var menus = document.querySelectorAll(".submenu");
+    menus.forEach(m => {
+        if (m.id !== menuId) m.style.display = "none";
+    });
+
+    var menu = document.getElementById(menuId);
+    if (menu.style.display === "block")
+        menu.style.display = "none";
+    else
+        menu.style.display = "block";
+}
