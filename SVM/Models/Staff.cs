374da43d15ad298f7bd9e8e1d947909df76634b1
pikachu1509 <bhagvatrahul0915@gmail.com>
2026-05-09 22:46:57 +0530

    Design staff and students page 9-5-26 time-10:47

--- SVM/Models/Staff.cs
+++ SVM/Models/Staff.cs
@@ -11,24 +11,32 @@ public partial class Staff
 
     public int? UserId { get; set; }
 
+    [Required(ErrorMessage = "Enter FirstName")]
     public string? FirstName { get; set; }
 
+    [Required(ErrorMessage = "Enter LastName")]
     public string? LastName { get; set; }
 
+    [Required(ErrorMessage = "Required Designation ")]
     public string? Designation { get; set; }
 
+    [Required(ErrorMessage = "Required Qualification ")]
     public string? Qualification { get; set; }
 
     public int? ExperienceYears { get; set; }
 
+    [Required(ErrorMessage = "Required JoiningDate ")]
     public DateTime? JoiningDate { get; set; }
 
     public decimal? Salary { get; set; }
 
+    [Required(ErrorMessage = "Required PhoneNumber ")]
     public string? Phone { get; set; }
 
+    [Required(ErrorMessage = "Required EmailId")]
     public string? Email { get; set; }
 
+    [Required(ErrorMessage = "Enter Address")]
     public string? Address { get; set; }
 
     public string? StafPhoto { get; set; }
