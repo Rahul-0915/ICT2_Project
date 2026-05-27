44eb862f465f98de450aa30443539a4b8658ae6f
pikachu1509 <bhagvatrahul0915@gmail.com>
2026-05-27 18:00:17 +0530

    Time-Table Create and Edit bug fix , then after add UserSide Controller :-index and about page :27-5-26

---
+++ SVM/Controllers/UserControllers/UserHomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SVM.Controllers.UserControllers
+{
+    public class UserHomeController : Controller
+    {
+        public IActionResult UserHome()
+        {
+            ViewBag.ShowPreloader = true;
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.ShowPreloader = false;
+            return View();
+        }
+
+
+    }
+}
